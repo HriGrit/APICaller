c8a36d2471f17291497bdf98de0e78cd05ddd051
HriGrit <86262322+HriGrit@users.noreply.github.com>
2023-10-20 02:00:35 +0530

    final final

--- src/components/UserSummary.jsx
+++ src/components/UserSummary.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import AvatarInitials from './AvatarInitials';
-import "./css/UserSummary.css";
+import "./UserSummary.css";
 
 function UserSummary({ user }) {
     const [userSummary, setUserSummary] = useState(null);
